ad845c32e5e10c77aa542e519ca1bacb66e5537a
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-10 13:50:38 +0000

    Commented out code until WMF 5.1 is RTM.

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,10 +8,11 @@ class wmf::params {
     '5.0' => {
       '2012' => 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K12-KB3134759-x64.msu',
       '2012 R2' => 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu',
-    },
-    '5.1' => {
-      '2012' => 'TODO',
-      '2012 R2' => 'TODO',
     }
+    # ,
+    # '5.1' => {
+    #   '2012' => 'TODO',
+    #   '2012 R2' => 'TODO',
+    # }
   }
 }
